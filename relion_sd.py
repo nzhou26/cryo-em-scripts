191b5465530dc5624c9759304603c4cf592bdeb8
nzhou26 <nzhou26@wisc.edu>
2021-06-16 14:49:34 +0800

    match mrc name by substring

--- relion_sd.py
+++ relion_sd.py
@@ -3,7 +3,9 @@ import starfile
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-
+import ntpath
+import numpy as np
+from IPython.display import clear_output
 if len(sys.argv) != 3:
     print("usage: ./relion_sd.py particles_after_ctf_refine.star particles_extract.star")
     exit()
@@ -15,7 +17,7 @@ names_non_dup = {val : key for key, val in temp.items()}
 
 mrc_list = names_non_dup.values()
 
-
+#print(df_old)
 dus = []
 dvs = []
 dur = []
@@ -23,16 +25,18 @@ dvr = []
 num_p = []
 ls = []
 idx = []
-
+i = 0
 for item in mrc_list:
-    select = df.loc[df['rlnMicrographName'] == item]
+    clear_output(wait=True)
+    select = df.loc[df['rlnMicrographName'].str.contains(ntpath.basename(item))]
     du_std = select['rlnDefocusU'].std()
     du_range = select['rlnDefocusU'].max() - select['rlnDefocusU'].min()
     dv_std = select['rlnDefocusV'].std()
     dv_range = select['rlnDefocusV'].max() - select['rlnDefocusV'].min()
     num_parts = len(select.index)
 
-    select_old = df_old.loc[df_old['rlnMicrographName'] ==item ]
+    select_old = df_old.loc[df_old['rlnMicrographName'].str.contains(ntpath.basename(item)) ]
+    #print(select_old)
     num_parts_old = len(select_old.index)
 
     loss = 1000 * (num_parts_old - num_parts) / num_parts_old
@@ -43,6 +47,8 @@ for item in mrc_list:
     num_p.append(num_parts)
     ls.append(loss)
     idx.append(item)
+    i = i + 1
+    print("Current progress:", np.round(i/len(mrc_list)*100, 2), "%")
 
 new_df = pd.DataFrame({"DefocusU_std" : dus,
                         "DefocusV_std" : dvs,
